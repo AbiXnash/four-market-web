94d41f3e8ddb2a61b8464da4ebcf2a0d6769eb26
Abinash <abinash@theabx.in>
2026-05-31 00:22:20 +0530

    removed all

--- src/api/auth/auth.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "@/lib/axios";
-import type { LoginRequest, LoginResponse } from "@/types/login";
-
-export const login =
-  async (request: LoginRequest) => {
-    return (await api.post<LoginResponse>("auth/login", request)).data
-  }
--- src/errors/AuthError.ts
+++
@@ -1,9 +0,0 @@
-export class AuthError extends Error {
-  code: string;
-
-  constructor(message: string, code: string) {
-    super(message);
-    this.name = "AuthError";
-    this.code = code;
-  }
-}
